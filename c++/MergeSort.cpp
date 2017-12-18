4706d942f04a7e2b0bf851f7e77ff8b5d03cfd1b
Abhik <abhiksark@gmail.com>
2017-12-18 13:08:14 +0530

    first commit

---
+++ c++/MergeSort.cpp
@@ -0,0 +1,46 @@
+/*
+abhiksarkar
+Merge Sort
+*/
+#include <stdio.h>
+#include <math.h>
+#include <string.h>
+#include <iostream>
+#include <vector>
+#include <list>
+#include <string>
+#include <algorithm>
+#include <queue>
+#include <stack>
+#include <set>
+#include <map>
+#include <complex>
+#define MAX_N 1000001
+using namespace std;
+typedef long long lld;
+
+int n;
+int in[MAX_N],tmp[MAX_N];
+void merge(int left,int mid,int right){
+	int h,i,j,k;
+	
+
+}
+void mergeSort(int left,int right){
+	if(left == right)
+		return;
+	int mid =(left + right )/2;
+	mergeSort(left,mid);
+	mergeSort(mid+1,right);
+	merge(left,mid,right);
+
+}
+
+int main(){
+	int n=8;
+	in[0]=4;in[1]=2;in[2]=1;in[3]=0;in[4]=5;in[5]=6;in[6]=9;in[7]=8;
+	mergeSort(0,n-1);
+	for(int i=0;i<n;i++)
+		cout<<in[i]<<endl;
+	return 0;
+}
