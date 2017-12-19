0301d7f94711d12d8d2e256e57c29e58b9d60320
Abhik <abhiksark@gmail.com>
2017-12-19 17:30:25 +0530

    added Heap Sort

--- c++/HeapSort.cpp
+++ c++/HeapSort.cpp
@@ -18,16 +18,52 @@ Merge Sort
 #define MAX_N 1000001
 using namespace std;
 typedef long long lld;
-void Heapify(int array[],int root,int size){
 
+
+void Heapify(int array[],int root,int size){
+    int left = 2 * root + 1, largest;
+    int right = left + 1, temp;
+
+    if(left < size && array[left] > array[root])
+        largest = left;
+    else
+        largest = root;
+
+    if(right < size && array[right] > array[largest])
+        largest = right;
+
+    if(largest != root){
+        temp = array[root];
+        array[root] = array[largest];
+        array[largest] = temp;
+        Heapify(array, largest, size);
+    }
+}
+
+void BuildHeap(int array[], int size){
+    for(int i = (size - 1) / 2; i >= 0; i--)
+        Heapify(array, i, size);
+}
+
+
+void HeapSort(int array[], int size){
+    BuildHeap(array, size);
+    int temp, i;
+
+    for(i = size - 1; i > 0; i--){
+        temp = array[0];
+        array[0] = array[i];
+        array[i] = temp;
+        Heapify(array, 0, i);
+    }
 }
 
 void PrintArray(int array[], int size){
     for(int i = 0; i < size; i++)
         cout << array[i] << " ";
-
     cout << endl;
 }
+
 int main(){
 	int array[] = {2, 4, 3, 1, 6, 8, 4};
 	HeapSort(array,7);
